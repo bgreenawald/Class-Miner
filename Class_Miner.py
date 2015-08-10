fe4ca27e3059ac9d9cfba14f4ddafc9697b8afff
Ben Greenawald <bgreenawald@gmail.com>
2015-08-10 15:15:02 -0400

    Completed file and bug fixes

--- Class_Miner.py
+++ Class_Miner.py
@@ -8,23 +8,30 @@ import ctypes  # An included library with Python install.
 
 def main():
     while(True):
+        one_time = 1 #Variable to ensure that the website is only checked once every five minutes
         now = datetime.now().time().isoformat().split(':')
         if int(now[1]) % 5 == 0:
-            cs3205()
-            cs2330()
-            econ2020()
+            if one_time == 1
+                cs3205()
+                cs2330()
+                econ2020()
+                one_time = 0
+
+        if int(now[1]) % 10 == 6 or int(now[1]) % 10 == 1:
+            one_time = 1
 
 
 def econ2020():
-    class_file = urllib.reequest.urlopen("view-source:https://rabi.phys.virginia.edu/mySIS/CS2/page.php?Semester=1158&Type=Group&Group=Economics")
+    class_file = urllib.request.urlopen("https://rabi.phys.virginia.edu/mySIS/CS2/page.php?Semester=1158&Type=Group&Group=Economics")
     for line in class_file:
-        if 'ECON2020' in str(line) and 'Lecture' in str(line) and 'Open' in str(line)
+        if 'ECON2020' in str(line) and 'Lecture' in str(line) and 'Open' in str(line):
+            ctypes.windll.user32.MessageBoxA(0, "O", "E",1)
 
 def cs3205():
     class_file = urllib.request.urlopen("https://rabi.phys.virginia.edu/mySIS/CS2/page.php?Semester=1158&Type=Group&Group=CompSci")
     for line in class_file:
         if 'CS3205' in str(line) and 'Open' in str(line):
-            ctypes.windll.user32.MessageBoxA(0, "O", "CS3205",1)
+            ctypes.windll.user32.MessageBoxA(0, "O", "C",1)
 
 
 def cs2330():
@@ -34,7 +41,7 @@ def cs2330():
         if 'CS2330' in str(line) and 'Enrollment Requirements: Undergraduate Engineering' not in str(line):
             counter += 1
             if counter > 4:
-                ctypes.windll.user32.MessageBoxA(0, "R", "CS2330",1)
+                ctypes.windll.user32.MessageBoxA(0, "R", "C",1)
 
 if __name__=='__main__':
     main()
